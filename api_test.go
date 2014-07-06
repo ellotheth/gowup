09ccf84dece1e1d15fc5a3db29f7ff5b53a791fb
ellotheth <ellotheth@users.noreply.github.com>
2014-07-05 21:19:59 -0600

    hey there are test suites!

--- api_test.go
+++ api_test.go
@@ -3,76 +3,82 @@ package guppy
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	//	"reflect"
 	"testing"
 )
 
-func TestSetHeaderDefaults(t *testing.T) {
-	api := WIU{Client: "herp", Token: "derp"}
-	req, _ := http.NewRequest("GET", "", nil)
-
-	api.setHeaders(req, nil)
-
-	assert.Equal(t, "application/json", req.Header.Get("Content-Type"), "should have a json content type")
-	assert.Equal(t, "Bearer herp derp", req.Header.Get("Auth"), "should set client auth")
+type ApiTest struct {
+	suite.Suite
+	server *httptest.Server
+	api    WIU
 }
 
-func TestSetCustomHeaders(t *testing.T) {
-	api := WIU{}
-	req, _ := http.NewRequest("GET", "", nil)
-
-	api.setHeaders(req, map[string]string{"herp": "derp", "bar": "foo"})
-
-	assert.Equal(t, "derp", req.Header.Get("herp"), "should set herp header")
-	assert.Equal(t, "foo", req.Header.Get("bar"), "should set bar header")
+func TestApi(t *testing.T) {
+	suite.Run(t, new(ApiTest))
 }
 
-func TestGet(t *testing.T) {
-	endpoint, client, token := "foo", "herp", "derp"
+func (apiSuite *ApiTest) SetupSuite() {
+	apiSuite.server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, _ := ioutil.ReadAll(r.Body)
+		w.Write(body)
+	}))
+	apiEntryPoint = apiSuite.server.URL
+	apiSuite.api = WIU{Client: "herp", Token: "derp"}
+}
 
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
-	defer server.Close()
+func (apiSuite *ApiTest) TearDownSuite() {
+	apiSuite.server.Close()
+}
 
-	apiEntryPoint = server.URL
-	api := WIU{Client: client, Token: token}
-	response, err := api.get(endpoint)
+func (apiSuite *ApiTest) TestSetHeaderDefaults() {
+	req, _ := http.NewRequest("GET", "", nil)
 
-	assert.Nil(t, err, "should not return an error")
+	apiSuite.api.setHeaders(req, nil)
+
+	apiSuite.Equal("application/json", req.Header.Get("Content-Type"), "should have a json content type")
+	apiSuite.Equal("Bearer herp derp", req.Header.Get("Auth"), "should set client auth")
+}
+
+func (apiSuite *ApiTest) TestSetCustomHeaders() {
+	req, _ := http.NewRequest("GET", "", nil)
+
+	apiSuite.api.setHeaders(req, map[string]string{"herp": "derp", "bar": "foo"})
+
+	apiSuite.Equal("derp", req.Header.Get("herp"), "should set herp header")
+	apiSuite.Equal("foo", req.Header.Get("bar"), "should set bar header")
+}
+
+func (apiSuite *ApiTest) TestGet() {
+	endpoint := "foo"
+
+	response, err := apiSuite.api.get(endpoint)
+
+	apiSuite.Nil(err, "should not return an error")
 
 	r := response.Request
-	assert.Equal(t, "GET", r.Method, "should be a GET")
-	assert.Equal(t, fmt.Sprintf("/%s", endpoint), r.URL.Path, "should be the right endpoint")
+	apiSuite.Equal("GET", r.Method, "should be a GET")
+	apiSuite.Equal(fmt.Sprintf("/%s", endpoint), r.URL.Path, "should be the right endpoint")
 
 	// todo: how do i confirm setHeaders was run?
 }
 
-func TestPost(t *testing.T) {
+func (apiSuite *ApiTest) TestPost() {
 	endpoint := "foo"
 	data := map[string]interface{}{"derp": "thing", "foo": []interface{}{"a", "string"}, "herp": 1}
 	marshaled, _ := json.Marshal(data)
 
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		body, _ := ioutil.ReadAll(r.Body)
-		w.Write(body)
-	}))
-	defer server.Close()
-
-	apiEntryPoint = server.URL
-	api := WIU{}
-
-	response, err := api.post(endpoint, data)
-	assert.Nil(t, err, "should not return an error")
+	response, err := apiSuite.api.post(endpoint, data)
+	apiSuite.Nil(err, "should not return an error")
 
 	body, _ := ioutil.ReadAll(response.Body)
 	defer response.Body.Close()
 
-	assert.Equal(t, "POST", response.Request.Method, "should be a POST")
-	assert.Equal(t, fmt.Sprintf("/%s", endpoint), response.Request.URL.Path, "should be the right endpoint")
-	assert.Equal(t, marshaled, body, "should post json data")
+	apiSuite.Equal("POST", response.Request.Method, "should be a POST")
+	apiSuite.Equal(fmt.Sprintf("/%s", endpoint), response.Request.URL.Path, "should be the right endpoint")
+	apiSuite.Equal(marshaled, body, "should post json data")
 
 	// apparently comparing maps is complicated. or json.Unmarshal is flaky.
 	// this comparison is USUALLY fine, but sometimes it fails for literally no
